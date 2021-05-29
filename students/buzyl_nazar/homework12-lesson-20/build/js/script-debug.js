cd7e7064968a21634f7695b6de8e04b274c3d42d
NazarBuzyl <nazarbuzyl@gmail.com>
2021-05-29 14:58:59 +0300

    bugFix

--- students/buzyl_nazar/homework12-lesson-20/build/js/script-debug.js
+++ students/buzyl_nazar/homework12-lesson-20/build/js/script-debug.js
@@ -6,8 +6,8 @@ var answer = "Привет, " + firstName + "!";
 console.log(answer);
 /* 2)   Запросите у пользователя год его рождения, посчитайте, сколько ему лет и выведите результат. Текущий год укажите в коде как константу.*/
 
-var birth_year = +prompt("Какой твой год рождения?"),
-    now = new Date();
+var birth_year = +prompt("Какой твой год рождения?");
+var now = new Date();
 var now_year = now.getFullYear();
 var age = now_year - birth_year;
 console.log(age);
@@ -18,35 +18,35 @@ var perimeter = side * 2;
 console.log(perimeter);
 /*4)   Запросите у пользователя радиус окружности и выведите площадь такой окружности.*/
 
-var radius = +prompt("Введи радиус окружности."),
-    pi = 314;
+var radius = +prompt("Введи радиус окружности.");
+var pi = 314;
 var area = radius * pi / 100;
 console.log(area);
 /*5)   Запросите у пользователя расстояние в км между двумя городами и за сколько часов он хочет добраться. Посчитайте скорость,
 с которой необходимо двигаться, чтобы успеть вовремя.*/
 
-var length = +prompt("Какое расстояние в км между двумя городами?"),
-    time = +prompt("За сколько часов хочешь добраться?");
+var length = +prompt("Какое расстояние в км между двумя городами?");
+var time = +prompt("За сколько часов хочешь добраться?");
 var speed = Math.floor(length / time) + " km/hour";
 console.log(speed);
 /*6)   Реализуйте конвертор валют. Пользователь вводит доллары, программа переводит в евро. Курс валюты храните в константе.*/
 
-var usd_eur = 82,
-    USD = +prompt("Введи цену в долларах.");
+var usd_eur = 82;
+var USD = +prompt("Введи цену в долларах.");
 var EUR = USD * usd_eur / 100;
 console.log(EUR);
 /*7)   Пользователь указывает объем флешки в Гб. Программа должна посчитать сколько файлов размером в 820 Мб помещается на флешку.*/
 
-var main_file = 820,
-    flesh_drive = +prompt("Какой объем твоей флешки?");
+var main_file = 820;
+var flesh_drive = +prompt("Какой объем твоей флешки?");
 var quantity_file = Math.floor(flesh_drive * 1000 / 820);
 console.log(quantity_file);
 /*8)   Пользователь вводит сумму денег в кошельке и цену одной шоколадки. Программа выводит сколько шоколадок может купить пользователь и сколько сдачи у него останется.*/
 
-var account = +prompt("Какая сумма в кошельке?"),
-    prise_choco = +prompt("Цена шоколадки?");
-var quantity_choco = Math.floor(account / prise_choco),
-    change = account - prise_choco * quantity_choco;
+var account = +prompt("Какая сумма в кошельке?");
+var prise_choco = +prompt("Цена шоколадки?");
+var quantity_choco = Math.floor(account / prise_choco);
+var change = account - prise_choco * quantity_choco;
 console.log(quantity_choco, change);
 /*9)   Запросите у пользователя трехзначное число и выведите его задом наперед. Для решения задачи вам понадобится оператор % (остаток от деления).*/
 
@@ -55,8 +55,8 @@ var message = number % 10 * 100 + Math.floor(number / 10) % 10 * 10 + Math.floor
 console.log(message);
 /*10)   Пользователь вводит сумму вклада в банк на 2 месяца, с процентной ставкой депозита 5% годовых. Вывести сумму начисленных процентов.*/
 
-var account_bank = +prompt("Какая сумма лежит в банке?"),
-    percentage = 5,
-    term = 2;
+var account_bank = +prompt("Какая сумма лежит в банке?");
+var percentage = 5;
+var term = 2;
 var accruals = account_bank * percentage / 1200 * term;
 console.log(accruals);
