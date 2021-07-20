1bbf79e495093242ae6a7a8c9646d975d463a488
Nazar Buzyl <nazarbuzyl@gmail.com>
2021-07-20 23:22:00 +0300

    add slider lightbox

--- students/buzyl_nazar/architecture/src/js/script.js
+++ students/buzyl_nazar/architecture/src/js/script.js
@@ -1,8 +1,39 @@
+import $ from 'jquery';
+import '../../node_modules/lightbox2/dist/js/lightbox-plus-jquery';
+import './slider';
+
+
 // see more
+// function 
+const htmlGallery = `
+<div class="col-6 col-xl-12">
+    <div class="row gallery__row">
+        <a href="image/gallery_photo04.jpg" data-lightbox="gallery-list-photo" class="gallery__link">
+            <img src="image/gallery_photo02.jpg" alt="photo 2" class="gallery__photo gallery__photo_left">
+        </a>
+        <a href="image/gallery_photo05.jpg" data-lightbox="gallery-list-photo" class="gallery__link">
+            <img src="image/gallery_photo03.jpg" alt="photo 3" class="gallery__photo">
+        </a>
+    </div>
+</div>
+<div class="col-6 col-xl-12">
+    <div class="row gallery__row">
+        <a href="image/gallery_photo04.jpg" data-lightbox="gallery-list-photo" class="gallery__link">
+            <img src="image/gallery_photo04.jpg" alt="photo 4" class="gallery__photo gallery__photo_left">
+        </a>
+        <a href="image/gallery_photo05.jpg" data-lightbox="gallery-list-photo" class="gallery__link">
+            <img src="image/gallery_photo05.jpg" alt="photo 5" class="gallery__photo">
+        </a>
+    </div>
+</div>
+`
 seeMore.addEventListener("click", function () {
     document.getElementById('more-photo').classList.add('active-flex');
-    document.getElementById('seeMore').classList.add('display-none');
+    $('#more-photo').append(htmlGallery);
+    // document.getElementById('seeMore').classList.add('display-none');
 });
+
+
 
 // btn burger
 let btn = document.querySelector('#burgerMenuBtnItem');
@@ -14,7 +45,5 @@ btn.addEventListener('click', function () {
 // scroll
 let scroll = document.querySelector('#scroll');
 scroll.addEventListener('click', function () {
-    
-})
-// // Splide
-// new Splide( '.splide' ).mount();
+
+});
---
+++ students/buzyl_nazar/architecture/src/js/slider.js
@@ -0,0 +1,67 @@
+import $ from 'jquery';
+import 'slick-carousel';
+import newsList from './lib/createNews';
+
+const slider = $('#newsList');
+let newsItemHtml = '';
+
+function getNewsHtml(item) {
+    let newsHtml = `
+    <div class="col-12">
+        <div class="news__card">
+            <a href="image/${item.img}" data-lightbox="news${item.id}">
+                <img src="image/${item.img}" alt="photo ${item.id}" class="news__photo">
+            </a>
+            <div class="news__info-block">
+                <strong class="news__strong">${item.title}</strong>
+                <p class="news__info">${item.info}</p>
+                <div class="row">
+                    <img src="image/${item.authorImg}" alt="${item.authorName}" class="news__main-photo">
+                    <div class="news__main-info">
+                        <p class="news__name">${item.authorName}</p>
+                        <p class="news__date">${item.date}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    `;
+    return newsHtml;
+};
+
+newsList.forEach(function (item) {
+    newsItemHtml += getNewsHtml(item);
+});
+console.log(newsItemHtml);
+
+slider.append(newsItemHtml);
+slider.slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    dots: true,
+    responsive: [
+        {
+            breakpoint: 967,
+            settings: {
+                slidesToShow: 2,
+            }
+        },
+        {
+            breakpoint: 767,
+            settings: {
+                slidesToShow: 1,
+                arrows: false,
+            }
+        }
+    ]
+});
+console.log(slider);
+
+// slider.on('beforeChange', function (event, slick, currentSlide, nextSlide) {
+//     $('.price-box').html(slick.$slides.eq(nextSlide).find('.slider__item').data('price'))
+// });
+
+// $('.go').on('click', function () {
+//     slider.slick('slickGoTo', 3);
+// })
