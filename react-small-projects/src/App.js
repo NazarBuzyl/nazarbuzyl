332078270cf0d28a0344c196e6d2e611d6acba13
Nazar Buzyl <nazarbuzyl@gmail.com>
2023-02-18 16:24:38 +0100

    create counter project

---
+++ react-small-projects/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Counter from "./Counter";
+import "./sass/_counter.scss";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div>
+        <Counter />
+      </div>
+    </div>
+  );
+}
---
+++ react-small-projects/src/Counter.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import "./sass/_counter.scss";
+
+export default function Counter() {
+  const [count, setCount] = useState(0);
+  const onClickAction = (action) => {
+    if (action) setCount(count - 1);
+    else setCount(count + 1);
+  };
+
+  return (
+    <div className="project-section counter ">
+      <h2 className="project-section__title">Counter:</h2>
+      <h3 className="counter__title">{count}</h3>
+      <div className="couunter__btn-block">
+        <button
+          className="counter__btn counter__btn_minus"
+          onClick={()=>onClickAction(1)}
+        >
+          - Minus
+        </button>
+        <button
+          className="counter__btn counter__btn_plus"
+          onClick={()=>onClickAction(0)}
+        >
+          Plus +
+        </button>
+      </div>
+    </div>
+  );
+}
